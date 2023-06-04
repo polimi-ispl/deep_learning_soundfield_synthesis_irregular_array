3ca1b1ba31ec646eec94402bbe6fa57d1116d9a0
Luca Comanducci <luca.comanducci@polimi.it>
2023-06-04 19:50:00 +0200

    complex network

--- train_lib/train_utils.py
+++ train_lib/train_utils.py
@@ -16,7 +16,6 @@ def normalize_graph(x):
     x_mean = tf.math.reduce_mean(x)
     x_std_dev = tf.math.sqrt(tf.math.reduce_variance(x))
     x_norm = (x - x_mean) / x_std_dev
-    #x_norm = x
     return x_norm #, x_mean, x_std_dev
 
 
@@ -58,119 +57,5 @@ def plot_to_image(figure):
     return image
 
 
-def est_vs_gt_filt_soundfield_fig(h_hat, h, h_min_val, h_max_val, SRG_inst, wc):
 
-    example_idx = tf.random.uniform(
-        shape=[1], minval=0, maxval=h_hat.shape[0], dtype=tf.int32)
-
-    h_hat_example = denormalize(h_hat.numpy()[example_idx], h_min_val, h_max_val)
-    h_example = denormalize(h.numpy()[example_idx], h_min_val, h_max_val)
-
-    # Need to reshape coeff in complex vector
-    h_hat_example = h_hat_example[:int(h_hat_example.shape[0] / 2)] + 1j * h_hat_example[
-                                                                           int(h_hat_example.shape[0] / 2):]
-    h_example = h_example[:int(h_example.shape[0] / 2)] + 1j * h_example[int(h_example.shape[0] / 2):]
-
-    P_est_val = SRG_inst.estimate_sounfield(wc, h_hat_example)
-    P_gt_val = SRG_inst.estimate_sounfield(wc, h_example)
-
-    figure_filt = plt.figure()
-    plt.subplot(221)
-    plt.title('Sounfield Re{} Est filters')
-    plt.imshow(np.real(P_est_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.subplot(222)
-    plt.title('Sounfield Im{} Est filters')
-    plt.imshow(np.imag(P_est_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.subplot(223)
-    plt.title('Sounfield Re{} Gt filters')
-    plt.imshow(np.real(P_gt_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.tight_layout()
-    plt.subplot(224)
-    plt.title('Sounfield Im{} Gt filters')
-    plt.imshow(np.imag(P_gt_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.tight_layout()
-
-    return figure_filt
-
-
-def est_vs_gt_soundfield(P_hat, P_gt):
-
-    # example_idx = tf.random.uniform(
-    #    shape=[1], minval=0, maxval=P_hat.shape[0], dtype=tf.int32)
-
-    example_idx = np.random.randint(low=0, high=P_hat.shape[0])
-
-
-    figure_sf = plt.figure(figsize=(10, 10))
-    plt.subplot(121)
-    plt.title('Est soundfield')
-    plt.imshow(np.real(P_hat.numpy()[example_idx, :, :, 0]), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.tight_layout()
-
-    plt.subplot(122)
-    plt.title('GT soundfield')
-    plt.imshow(np.real(P_gt.numpy()[example_idx, :, :, 0]), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.tight_layout()
-
-    return figure_sf
-
-
-def est_vs_gt_filt_soundfield_fig_train_2(h_hat, h, h_min_val, h_max_val, SRG_inst, wc, mask):
-
-    # example_idx = tf.random.uniform(
-    #    shape=[1], minval=0, maxval=h_hat.shape[0], dtype=tf.int32)
-
-    example_idx = np.random.randint(low=0, high=h_hat.shape[0])
-
-
-    h_hat_example = denormalize(h_hat.numpy()[example_idx], h_min_val, h_max_val)
-    h_example = denormalize(h.numpy()[example_idx], h_min_val, h_max_val)
-
-    h_hat_example_hole = np.multiply(h_hat_example, mask.numpy()[example_idx])
-    h_example_hole = np.multiply(h_example, mask.numpy()[example_idx])
-
-    # Need to reshape coeff in complex vector
-    h_hat_example_hole = h_hat_example_hole[:int(h_hat_example_hole.shape[0] / 2)] + 1j * h_hat_example_hole[
-                                                                           int(h_hat_example_hole.shape[0] / 2):]
-    h_example_hole = h_example_hole[:int(h_example_hole.shape[0] / 2)] + 1j * h_example_hole[
-                                                                                          int(h_example_hole.shape[
-                                                                                                  0] / 2):]
-    h_example = h_example[:int(h_example.shape[0] / 2)] + 1j * h_example[int(h_example.shape[0] / 2):]
-
-    P_est_val = SRG_inst.estimate_sounfield(wc, h_hat_example_hole)
-    P_gt_val = SRG_inst.estimate_sounfield(wc, h_example)
-    P_gt_val_hole = SRG_inst.estimate_sounfield(wc, h_example_hole)
-
-    figure = plt.figure(figsize=(20, 10))
-    plt.subplot(231)
-    plt.title('Sounfield Re{} Est filters')
-    plt.imshow(np.real(P_est_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.xlabel('x [m]'), plt.ylabel('y [m]')
-    plt.tight_layout()
-    plt.subplot(232)
-    plt.title('Sounfield Re{} Gt filters holes')
-    plt.imshow(np.real(P_gt_val_hole), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.xlabel('x [m]'), plt.ylabel('y [m]')
-    plt.tight_layout()
-    plt.subplot(233)
-    plt.title('Sounfield Re{} Gt filters')
-    plt.imshow(np.real(P_gt_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.xlabel('x [m]'), plt.ylabel('y [m]')
-    plt.tight_layout()
-
-    plt.subplot(234)
-    plt.title('Sounfield Im{} Est filters')
-    plt.imshow(np.imag(P_est_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.xlabel('x [m]'), plt.ylabel('y [m]')
-    plt.subplot(235)
-    plt.title('Sounfield Im{} Gt filters holes')
-    plt.imshow(np.imag(P_gt_val_hole), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.xlabel('x [m]'), plt.ylabel('y [m]')
-    plt.tight_layout()
-    plt.subplot(236)
-    plt.title('Sounfield Im{} Gt filters')
-    plt.imshow(np.imag(P_gt_val), aspect='auto'), plt.colorbar(), plt.gca().invert_yaxis()
-    plt.xlabel('x [m]'), plt.ylabel('y [m]')
-    plt.tight_layout()
-    return figure
 
